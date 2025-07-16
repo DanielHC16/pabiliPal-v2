af950de0f83fc14b2870ae2b2b874600bb27aa41
Daniel Camacho <danielcamacho0416@gmail.com>
2025-07-16 17:42:11 +0800

    Bugfix - Arcjet Node Version Update

--- backend/lib/arcjet.js
+++ backend/lib/arcjet.js
@@ -1,23 +1,7 @@
-import arcjet, { tokenBucket, shield, detectBot} from "@arcjet/node";
-
-import "dotenv/config";
-
-// init arcjet
+import arcjet from "@arcjet/node";
 
 export const aj = arcjet({
-    key: process.env.ARCJET_KEY,
-    characteristics:["ip.src"],
-    rules:[
-        shield({mode:"LIVE"}), // Protection for our web app (SQL Injfection, XSS, CSRF)
-        detectBot({
-            mode:"LIVE",
-            allow:["CATEGORY:SEARCH_ENGINE"] // Block all bots except search engines
-        }),
-        tokenBucket({
-            mode:"LIVE",
-            refillRate: 5,
-            interval: 10,
-            capacity: 10,
-        }) // RATE LIMITING
-    ]
-})
+  key: process.env.ARCJET_KEY,
+  characteristics: [],
+  rules: [] // leave empty for now
+});
